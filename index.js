dd9b701c31053228d735cfe1b9cfa92631754eb2
Danielle Matton <danimatton@gmail.com>
2019-09-23 21:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var counter;
+
+function theBeatlesPlay() {
+  for() {
+    
+  }
+}
+
+var facts = ["He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"];
+        
+function johnLennonFacts() {
+  while(facts <= 3) {
+    
+  }
+  return
+}
+
+function iLoveTheBeatles() {
+  var = [];
+}
