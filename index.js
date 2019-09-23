f62e5273c86dd0c78231cebf22e7ffdcd40bc8ed
Danielle Matton <danimatton@gmail.com>
2019-09-23 21:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for(var i = 0; i < musicians.length; i++) {
-  }
   array.push(musicians[i] + ' plays ' + instruments[i])
+}
+  return array;
 }
 
 var facts = ["He was the last Beatle to learn to drive",
