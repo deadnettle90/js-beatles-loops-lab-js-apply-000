28078bbeea72e471e89b1b8b5afcfafdd26941d2
Danielle Matton <danimatton@gmail.com>
2019-09-23 22:25:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-var facts = ["He was the last Beatle to learn to drive",
+
+        
+function johnLennonFacts(facts) {
+  var facts = ["He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"];
-        
-function johnLennonFacts(facts) {
   message = [ ];
   count = 0;
   while (count < facts.length) {
